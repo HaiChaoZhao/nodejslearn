8a85ab5f4aebc3f9944f5965929548f707cc7051
赵海朝 <847204786@qq.com>
2018-05-14 17:47:06 +0800

    添加中文uri解析

--- src/helper/route.js
+++ src/helper/route.js
@@ -13,11 +13,11 @@ const source = fs.readFileSync(tplPath);
 const template = Handlebars.compile(source.toString());
 module.exports = async function (req, res, filePath) {
     try {
-        const stats = await stat(filePath);
+        const stats = await stat(decodeURI(filePath));
         if (stats.isFile()) {
             res.statusCode = 200;
             res.setHeader('Content-Type', mime(filePath));
-            let rs=fs.createReadStream(filePath);
+            let rs=fs.createReadStream(decodeURI(filePath));
             if(filePath.match(config.compress)){
                 rs=compress(rs,req,res);
             }
