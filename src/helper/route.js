79a7294affcb632fc8379c9f75aaddfef03833ce
赵海朝 <847204786@qq.com>
2018-05-14 18:04:20 +0800

    范围访问添加完毕

--- src/helper/route.js
+++ src/helper/route.js
@@ -7,6 +7,7 @@ const readdir = promisify(fs.readdir);
 const config = require('../config/defaultConfig');
 const mime=require('./mime');
 const compress=require('./compress');
+const range=require('./range');
 
 const tplPath = path.join(__dirname, '../template/dir.html');
 const source = fs.readFileSync(tplPath);
@@ -15,9 +16,17 @@ module.exports = async function (req, res, filePath) {
     try {
         const stats = await stat(decodeURI(filePath));
         if (stats.isFile()) {
-            res.statusCode = 200;
+            
             res.setHeader('Content-Type', mime(filePath));
-            let rs=fs.createReadStream(decodeURI(filePath));
+            let rs;
+            const {code,start,end}=range(stats.size,req,res);
+            if(code===200){
+                res.statusCode = 200;
+                rs=fs.createReadStream(decodeURI(filePath));
+            }else{
+                res.statusCode = 206;
+                rs=fs.createReadStream(decodeURI(filePath),{start,end});
+            }
             if(filePath.match(config.compress)){
                 rs=compress(rs,req,res);
             }
