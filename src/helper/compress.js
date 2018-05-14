7ca11288653805df7cadb90245ec8a8cd7bc38a4
赵海朝 <847204786@qq.com>
2018-05-14 17:26:23 +0800

    gizp改为gzip

--- src/helper/compress.js
+++ src/helper/compress.js
@@ -7,7 +7,7 @@ module.exports = (rs, req, res) => {
     if (!acceptEncoding || !acceptEncoding.match(/\b(gzip|deflate)\b/)) {
         return rs;
     } else if (acceptEncoding.match(/\bgzip\b/)) {
-        res.setHeader('Content-Encoding', 'gizp');
+        res.setHeader('Content-Encoding', 'gzip');
         return rs.pipe(createGzip());
     } else if (acceptEncoding.match(/\bdeflate\b/)) {
         res.setHeader('Content-Encoding', 'deflate');
